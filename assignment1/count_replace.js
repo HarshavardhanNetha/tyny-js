9be9ea107041b8b475b54ab8d25c18e6ee641bc0
harsha <harshavardhang123@gmail.com>
2022-05-25 17:18:27 +0530

    inbuild method lastIndexOf

--- assignment1/count_replace.js
+++ assignment1/count_replace.js
@@ -1,24 +1,29 @@
 const arr = ["one","two","one","three","one","three"]
 let key = "one"
-let obj = {}
-arr.forEach(i => {
-    if(obj[i]== undefined){
-        obj[i] = 1
-    }
-    else{
-        obj[i] += 1
-    }
-})
 
-const rev_arr = arr.reverse()
+arr[arr.lastIndexOf(key)] = '1'
+
+console.log(arr)
+
+// let obj = {}
+// arr.forEach(i => {
+//     if(obj[i]== undefined){
+//         obj[i] = 1
+//     }
+//     else{
+//         obj[i] += 1
+//     }
+// })
+
+// const rev_arr = arr.reverse()
 // console.log(rev_arr)
 
 // for( const key in obj){
-if(obj[key]>1){
-    // find index of key in array and update it to 1
-    let index = rev_arr.indexOf(key)
-    rev_arr[index] = "1"
-}
+// if(obj[key]>1){
+//     // find index of key in array and update it to 1
+//     let index = rev_arr.indexOf(key)
+//     rev_arr[index] = "1"
 // }
+// // }
 
-console.log(rev_arr.reverse())
+// console.log(rev_arr.reverse())
