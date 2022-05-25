528379d13479c49688fd431ba1b881dbe5d59994
harsha <harshavardhang123@gmail.com>
2022-05-25 17:05:37 +0530

    updates

--- assignment1/count_replace.js
+++ assignment1/count_replace.js
@@ -1,4 +1,5 @@
 const arr = ["one","two","one","three","one","three"]
+let key = "one"
 let obj = {}
 arr.forEach(i => {
     if(obj[i]== undefined){
@@ -7,18 +8,17 @@ arr.forEach(i => {
     else{
         obj[i] += 1
     }
-}
-)
+})
 
 const rev_arr = arr.reverse()
 // console.log(rev_arr)
 
-for( const key in obj){
-    if(obj[key]>1){
-        // find index of key in array and update it to 1
-        let index = rev_arr.indexOf(key)
-        rev_arr[index] = "1"
-    }
+// for( const key in obj){
+if(obj[key]>1){
+    // find index of key in array and update it to 1
+    let index = rev_arr.indexOf(key)
+    rev_arr[index] = "1"
 }
+// }
 
 console.log(rev_arr.reverse())
--- assignment1/file_extension.js
+++ assignment1/file_extension.js
@@ -1,4 +1,6 @@
 let filename = "Madhav.mp4"
 
 const arr = filename.split(".")
-console.log(arr[arr.length-1])
+//console.log(arr[arr.length-1])
+// console.log(arr.slice(-1))
+console.log(arr.pop())
